3555d22c2e3cd6869e8a869bef003e31698a4759
Marc <32514554+0xjacobb@users.noreply.github.com>
2019-05-10 16:16:50 +0200

    Create CREATE_twitter_credentials.py

---
+++ CREATE_twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "YOUR KEY"
+ACCESS_TOKEN_SECRET = "YOUR KEY"
+CONSUMER_KEY = "YOUR KEY"
+CONSUMER_SECRET = "YOUR KEY"

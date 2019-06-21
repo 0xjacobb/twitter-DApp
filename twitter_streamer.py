e42ae8044320738de0a7fc8f584dc60cebf612c5
Marc Steiner <spare@gmx.chr>
2019-06-21 15:20:43 +0200

    refactoring, delete not needed code, commenting

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -7,40 +7,11 @@ import json
 import re
 
 import twitter_credentials
-import numpy as np
-import pandas as pd
 from web3 import Web3, HTTPProvider
 
 '''
-TWITTER CLIENT
-Data from one twitter account
-'''
-
-
-class TwitterClient():
-
-    def __init__(self, twitter_user=None):
-        self.auth = TwitterAuthenticator().authenticate_twitter_app()
-        self.twitter_client = API(self.auth)
-        self.twitter_user = twitter_user
-
-    '''
-    Grab number_of_tweets from the timeline of the specified user
-    if twitter_user=None --> goes to your own timeline
-    '''
-
-    def get_user_timeline_tweets(self, number_of_tweets):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(number_of_tweets):
-            tweets.append(tweet)
-        return tweets
-
-    def get_twitter_client_api(self):
-        return self.twitter_client
-
-
-'''
 TWITTER AUTHENTICATOR
+Handels connection to Twitter API
 '''
 
 
@@ -95,14 +66,13 @@ class TwitterListener(StreamListener):
         self.fetched_tweets_filename = fetched_tweets_filename
         self.i = 0
 
-    # Method who takes the data (listening to tweets)
+    # Method who takes the data (listening to tweets) and interact with Smart Contract
     def on_data(self, raw_data):
          # TwitterStreamer.sol Smart Contract address which was provided during `truffle deploy`
-        contract_address = '<FILL IN CONTRACT ADDRESS>'
-        # Private Key`
-        private_key = '<FILL IN PRIVATE KEY>'
-        # Adress TBD
-        address_for = '<FILL IN ADDRESS>'
+        contract_address = '<FILL IN CONTRACT ADDRESS SHOWN AFTER TRUFFLE DEPLOY>'
+
+        # Address which receives the TST Tokken
+        receiver_address = '<FILL IN RECIPIENT ADDRESS>'
 
         try:
             json_load = json.loads(raw_data)
@@ -112,38 +82,43 @@ class TwitterListener(StreamListener):
             print("########## NEW TWEET: Nr: %i ########## \n" %
                   (self.i), s[2:-1])
 
+            self.i += 1
             #print("Address is: ", re.search("0x.{40}",s).group())
             #receiver_address = re.search("0x.{40}",s).group()
-
-            self.i += 1
-            text = json_load['geo']
-            s = str(text)
-            print("LOCATION", s)
-            print()
 
             with open("./contractJSONABI.json") as f:
                 info_json = json.load(f)
             abi = info_json
             w3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
-            free_tokkens = w3.eth.contract(address=contract_address, abi=abi,)
+            free_tokkens_instance = w3.eth.contract(
+                address=contract_address, abi=abi,)
 
-            # set sender account (Account from ganache)
+            '''
+            set sender account (Account from ganache). 
+            Account [0] is the private key of the Smart Contract owner. Truffle deploy uses ganache-cli
+            ganache-cli has the private key implmented in the node. Default key: [0]
+            '''
             w3.eth.defaultAccount = w3.eth.accounts[0]
-            # send message to contract
+
+            '''send message to contract. If it is not working with recevier_address try
+            tx_hash = free_tokkens_instance.functions.mintToken('0x9b26a3C40d32BD9e40266711Fd89ea9387340E90').transact()
+            '''
             print('Get some Tokens...')
-            tx_hash = free_tokkens.functions.mintToken(
-                address_for).transact()
-            # tx_hash = free_tokkens.functions.mintToken('0x9b26a3C40d32BD9e40266711Fd89ea9387340E90').transact()
+            tx_hash = free_tokkens_instance.functions.mintToken(
+                receiver_address).transact()
 
             # Wait for transaction to be mined...
             w3.eth.waitForTransactionReceipt(tx_hash)
 
-            # Read out message
-            #print('Balance: {}'.format(free_tokkens.functions.balanceOf('0x9b26a3C40d32BD9e40266711Fd89ea9387340E90').call()))
-            print('Balance: {}'.format(free_tokkens.functions.balanceOf(
-                address_for).call()))
+            '''
+            Read out the balance of the recipient
+            If it is not working with recevier_address try
+            print('Balance: {}'.format(free_tokkens_instance.functions.balanceOf('0x9b26a3C40d32BD9e40266711Fd89ea9387340E90').call()))
+            '''
+            print('Balance: {}'.format(
+                free_tokkens_instance.functions.balanceOf(receiver_address).call()))
 
-            # Save to file
+            # Save tweets to file for analysis if needed
             with open(self.fetched_tweets_filename, 'a') as tf:
                 tf.write(raw_data)
             return True
@@ -152,7 +127,7 @@ class TwitterListener(StreamListener):
             print("Error on raw_data: %s" % str(e))
         return True
 
-    # Method who handles errors
+    # Method who handles Twitter API errors
     def on_error(self, status_code):
         '''
         402 error from twitter API = hit the rate limit. You have to wait
@@ -163,28 +138,7 @@ class TwitterListener(StreamListener):
         print(status_code)
 
 
-'''
-Analyzing content from tweets and store it in a dataframe
-'''
-
-
-class TweetAnalzer():
-    def tweets_to_data_frame(self, tweets):
-        df = pd.DataFrame(
-            data=[tweet.text for tweet in tweets], columns=['Tweets'])
-
-        df['id'] = np.array([tweet.id for tweet in tweets])
-        df['len'] = np.array([len(tweet.text) for tweet in tweets])
-        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
-        df['source'] = np.array([tweet.source for tweet in tweets])
-        df['geo'] = np.array([tweet.geo for tweet in tweets])
-
-        return df
-
-
 if __name__ == "__main__":
-
-    '''STREAMER PART'''
     hash_tag_list = ["giveMeTSTToken"]
     fetched_tweets_filename = "tweets.txt"
     twitter_streamer = TwitterStreamer()
